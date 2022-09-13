3791da22c2cd1f9108a1f7264a1071d5abd7b4d8
JoKyungseo <eksql0645@gmail.com>
2022-09-13 09:31:29 +0900

    [Feat] 초기 설정

---
+++ utils/errorCodes.js
@@ -0,0 +1,7 @@
+// 자주 사용되는 Error Codes의 관리를 위한 파일
+module.exports = {
+  required: "필수 값입니다.",
+  pageNotFound: "페이지를 찾을 수 없습니다.",
+  thereIsNotOrder: "주문내역이 존재하지 않습니다.",
+  serverError: "서버 에러가 발생하였습니다. 잠시 후 시도하여 주세요.",
+};
