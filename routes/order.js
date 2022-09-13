d1b4ffc5894899b0e300c03b630c6b0a465985cd
JoKyungseo <eksql0645@gmail.com>
2022-09-13 09:45:11 +0900

    [Feat] 주문 조회 API 구현

--- routes/order.js
+++ routes/order.js
@@ -3,4 +3,6 @@ const router = Router();
 const { orderService } = require("../service");
 
 router.post("/", orderService.addOrder);
+router.get("/:orderNum", orderService.getOrder);
+
 module.exports = router;
